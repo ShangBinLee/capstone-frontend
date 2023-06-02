c777ae4d56e4039239bcd9272cb55ebd6e333177
LeeShangBin <dkfkgks64@gmail.com>
2023-06-03 03:32:45 +0900

    feature : Add import of BookCard in App.js and sample of its info

--- src/App.js
+++ src/App.js
@@ -8,7 +8,33 @@ import Emailauthnum from './pages/EmailAuthnum.js';
 import Footer from './headfoot/footer.js';
 import Header from './headfoot/header.js'
 import Album from './pages/boardpage.js';
+import BookCard from './components/BookCard.js';
 // import Card from "./pages/Card.js"
+
+const bookCardInfoSample = {
+  mainInfo : {
+    img : {
+      width : '115.86',
+      height : '168',
+      src : 'https://img.khan.co.kr/news/2023/04/22/news-p.v1.20230422.8ce451d8c991486698c99a002ecbd65f_P1.jpg'
+    },
+    infoText : {
+      title : '스즈메의 문단속',
+      autor : '신카이 마코토',
+      price : 1800000,
+      date : '2023.04.16'
+    }
+  },
+  detailInfo : {
+    department : '아트&디자인 테크놀로지 대학',
+    lecture : '디지털 공학',
+    professor : '장준혁 교수',
+    seller : {
+      name : '신현수',
+      rating : 10
+    }
+  }
+};
 
 function App() {
   return (
@@ -19,8 +45,9 @@ function App() {
     {/* <Emailauthrequ/> */}
     {/* <Emailauthnum/> */}
     {/* <Album/> */}
-    <Album/>
-    <Footer/>
+      {/* <Album/> */}
+      {/* {<BookCard bookCardInfo={bookCardInfoSample} />} */}
+      <Footer/>
     </Grid>
   );
 }
