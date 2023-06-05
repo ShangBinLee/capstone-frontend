6469a167b9b6ece4610942f652e399272a2c7790
sinbak <sinbato03@daum.net>
2023-06-06 02:43:28 +0900

    feature: change arrow img into arrowicon

--- src/components/ToggleArrow.js
+++ src/components/ToggleArrow.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './ToggleArrow.module.css';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const toggleArrow = (props) => {
     return (
     <div className={styles.toggle_arrow} onClick={props.onClick}>
         <button className={`${styles.btn_toggle_detail} ${props.opened ? '' : styles.fold}`}>
-            <img src="img/expand-arrow.png" alt=""></img>
+            <ExpandMoreIcon/>
         </button>
     </div>
     );
