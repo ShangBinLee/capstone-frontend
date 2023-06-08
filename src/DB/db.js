e5913fb9e5ff55d00a1102ff9c83927bc8fc5b86
ShangBin <dkfkgks64@gmail.com>
2023-06-08 10:41:19 +0900

    chore : Add src/DB/db.js, Define bit_chat db

---
+++ src/DB/db.js
@@ -0,0 +1,7 @@
+import Dexie from 'dexie';
+
+export const db = new Dexie('bit_chat');
+db.version(1).stores({
+  chat_room: 'id, modified_date, product_id',
+  chat : 'id, chat_content, create_date, modified_date, chat_room_id, sender_id'
+});
