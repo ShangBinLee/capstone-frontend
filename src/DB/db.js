98fff560ecf6ca953941a3fad363022cca6f02e3
ShangBin <dkfkgks64@gmail.com>
2023-06-11 22:35:05 +0900

    chore : Add offer table to db schema

--- src/DB/db.js
+++ src/DB/db.js
@@ -3,5 +3,6 @@ import Dexie from 'dexie';
 export const db = new Dexie('bit_chat');
 db.version(1).stores({
   chat_room: 'id, modified_date, product_id, buyer_id',
-  chat : 'id, chat_content, create_date, modified_date, chat_room_id, sender_id'
+  chat : 'id, chat_content, create_date, modified_date, chat_room_id, sender_id',
+  offer : 'id, offer_price, offerer_id' // id는 chat_room_id 값을 저장
 });
