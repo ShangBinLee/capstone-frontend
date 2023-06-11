16172d2d43045398344083403e448fd40f8c29e8
ShangBin <dkfkgks64@gmail.com>
2023-06-11 22:12:31 +0900

    chore : Update chat db schema

--- src/DB/db.js
+++ src/DB/db.js
@@ -2,6 +2,6 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('bit_chat');
 db.version(1).stores({
-  chat_room: 'id, modified_date, product_id',
+  chat_room: 'id, modified_date, product_id, buyer_id',
   chat : 'id, chat_content, create_date, modified_date, chat_room_id, sender_id'
 });
