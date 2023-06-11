c8c2440992f45ea832918b39e32fefa8a5f4f922
ShangBin <dkfkgks64@gmail.com>
2023-06-11 22:28:13 +0900

    fix : Fix wrong destructuring alignment in getChatHistory

--- src/socket/handler/chat_handler.js
+++ src/socket/handler/chat_handler.js
@@ -1,7 +1,7 @@
 import { db } from "../../DB/db"
 
 const getChatHistory = ({ chat_room_id, chat_history }) => {
-    chat_history.forEach(({ chat_room_id : id, ...others}) =>{
+    chat_history.forEach(({ chat_id : id, ...others}) =>{
         db.chat.put({
             id,
             ...others
