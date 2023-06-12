30b19ec50683c88fb8e58580e2f40f80ca675a4d
ShangBin <dkfkgks64@gmail.com>
2023-06-12 12:26:12 +0900

    update : Remove import for fs, Add import for socket_config.js

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
-import { readFileSync } from 'fs';
-
-const config = JSON.parse(readFileSync('./socket_config.json'));
-const url = config.url[process.NODE_ENV];
+import { url } from './socket_config.js';
 
 export const socket = io(url, {
     autoConnect : false
