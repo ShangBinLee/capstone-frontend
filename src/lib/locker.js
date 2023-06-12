bf096a215c8662427173d79cbd92c63c5d7d444d
ShangBin <dkfkgks64@gmail.com>
2023-06-12 11:41:23 +0900

    feature : Implement assignLocker

--- src/lib/locker.js
+++ src/lib/locker.js
@@ -1,3 +1,5 @@
+import { fetchAPIWithAuth } from "./fetch_api";
+
 const checkLockerUnassigned = (buildingNum, rootUrl) => {
     return fetch(`${rootUrl}/api/web/locker/unassigned`, {
         method : 'POST',
@@ -10,6 +12,11 @@ const checkLockerUnassigned = (buildingNum, rootUrl) => {
     .then((result) => result.data);
 };
 
+const assignLocker = (buyerStudentId, productId, lockerId, rootUrl) => {
+    return fetchAPIWithAuth(`${rootUrl}/api/web/locker/assign/${buyerStudentId}/${productId}/${lockerId}`);
+};
+
 export {
-    checkLockerUnassigned
+    checkLockerUnassigned,
+    assignLocker
 };
