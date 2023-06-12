33c7437a59c189f7b37608f61764f24bcc41e3f6
LeeShangBin <dkfkgks64@gmail.com>
2023-06-13 07:40:46 +0900

    refactor : Remove unused variables, Make App initializeSocket

--- src/App.js
+++ src/App.js
@@ -1,80 +1,60 @@
 import { rootUrl } from './config.js';
-import React from 'react'
+import { React, useEffect } from 'react'
 import "./App.css"
-import Grid from '@mui/material/Grid';
 import Signin from './pages/Signin.js'
 import Signup from './pages/Signup.js';
 import Emailauthrequ from './pages/EmailAuthrequ.js';
 import Emailauthnum from './pages/EmailAuthnum.js';
-import Footer from './headfoot/footer.js';
-import Header from './headfoot/header.js'
 import Boardpage from './pages/boardpage.js';
-import BookCard from './components/BookCard.js';
-import bookCardInfoSample from './pages/bookCardInfoSample.js';
-import SelectLabels from './pages/Selectlabel.js';
-import Searchbar from './pages/Searchbar.js';
-import Searchpage from './pages/Searchpage.js';
+// import Searchpage from './pages/Searchpage.js';
 import Detail from './pages/Detailpage.js';
 import ReactDOM from 'react-dom';
 import Mypage from './pages/Mypage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MySaleOrderList from './components/MySaleOrderList.js';
 import MyBuyOrderList from './components/MyBuyOrderList.js'
-// import Card from "./pages/Card.js"
+import ChatRoomList from './components/ChatRoomList.js';
+import ChatRoomDetail from './components/ChatRoomDetail.js';
+import { socket } from './socket/socket.js';
+import { initializeSocket } from './socket/init.js';
+import './DB/db.js';
 
 ReactDOM.render(
     <Router>
-      <div className="app-router">
-        <Routes>
-          <Route exact path="/" element={<Signin/>} />
-          <Route exact path="/emailauthrequ" element={<Emailauthrequ/>} />
-          <Route path="/emailauthnum" element={<Emailauthnum/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/boardpage" element={<Boardpage rootUrl={rootUrl}/>} />
-          <Route path="/detail/:productId" element={<Detail rootUrl={rootUrl}/>}/>
-          <Route path="/mypage" element={<Mypage/>}/>
-          <Route path="/mypage/buy/list" element={<MyBuyOrderList rootUrl={rootUrl}/>}/>
-          <Route path="/mypage/sale/list" element={<MySaleOrderList rootUrl={rootUrl}/>}/>
-        </Routes>
-      </div>
+      <App></App>
     </Router>
   ,
   document.getElementById('root')
 );
 
 function App() {
+  useEffect(() => {
+    if(localStorage.getItem('authorization') !== undefined) {
+      initializeSocket(rootUrl);
+    }
+
+    return () => socket.disconnect();
+  }, []);
+
   return (
-    <Grid container justifyContent = "center" height="100vh" flexDirection="column">
-      
-      {/* <Routes>
-        <Route exact path="/" element={<Signin/>} />
-        <Route exact path="/emailauthrequ" element={<Emailauthrequ/>} />
-        <Route path="/emailauthrnum" element={<Emailauthnum/>} />
-        <Route path="/boardpage" element={<Boardpage/>} />
-      </Routes> */}
-
-      {/* <Header/> */}
-      {/* <Routes>
-        <Route path="/" element={<Signin/>}/>
-      </Routes> */}
-    {/* <Signin/> */}
-    {/* <Signup/> */}
-    {/* <Emailauthrequ/> */}
-    {/* <Emailauthnum/> */}
-    {/* <Album/> */}
-      {/* <Boardpage/> */}
-      {/* {<BookCard bookCardInfo={bookCardInfoSample} />} */}
-      {/* <Footer/> */}
-    {/* <Boardpage bookCardInfo={bookCardInfoSample}/> */}
-    {/* <Detail/> */}
-    {/* <Searchbar/> */}
-    {/* <Grid item><Searchpage/></Grid> */}
-    
-    {/* <Footer/> */}
-    </Grid>
+    <div className="app-router">
+        <Routes>
+          <Route exact path="/" element={<Signin rootUrl={rootUrl}/>} />
+          <Route exact path="/emailauthrequ" element={<Emailauthrequ/>} />
+          <Route path="/emailauthnum" element={<Emailauthnum/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/boardpage" element={<Boardpage rootUrl={rootUrl}/>} />
+          <Route path="/detail/:productId" element={<Detail rootUrl={rootUrl}/>}/>
+          <Route path="/mypage" element={<Mypage rootUrl={rootUrl}/>}/>
+          <Route path="/mypage/buy/list" element={<MyBuyOrderList rootUrl={rootUrl}/>}/>
+          <Route path="/mypage/sale/list" element={<MySaleOrderList rootUrl={rootUrl}/>}/>
+          <Route path="/chat-room">
+            <Route path="list" relative="path" element={<ChatRoomList rootUrl={rootUrl}/>}/>
+            <Route path=":chatRoomId" relative="path" element={<ChatRoomDetail rootUrl={rootUrl}/>}/>
+          </Route>
+        </Routes>
+      </div>
   );
-}
-
-
+};
 
 export default App;
