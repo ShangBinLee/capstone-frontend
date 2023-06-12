c4adbd18e935abe7199b5f90a63abf26388104ca
LeeShangBin <dkfkgks64@gmail.com>
2023-06-13 07:37:51 +0900

    refactor : Remove unused variables in footer.js

--- src/headfoot/footer.js
+++ src/headfoot/footer.js
@@ -5,20 +5,11 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import ListRoundedIcon from '@mui/icons-material/ListRounded';
 import ForumRoundedIcon from '@mui/icons-material/ForumRounded';
-import Boardpage from '../pages/boardpage.js';
-import bookCardInfoSample from '../pages/bookCardInfoSample.js';
 import { useNavigate } from 'react-router-dom';
-import { Link } from '@mui/material';
-import Signin from '../pages/Signin';
 
 function Footer() {
-    // const [value, setValue] = React.useState('recents');
     const navigate = useNavigate();
-    const [value, setValue] = React.useState('/');
-  
-    // const handleChange = (event, newValue) => {
-    //   setValue(newValue);
-    // };
+    const [value] = React.useState('/');
 
     const handlePageChange = (page) => {
       navigate(page);
@@ -26,16 +17,7 @@ function Footer() {
 
       return ( 
       <div>
-      {/* 라우팅하는 페이지 */}
-      {/* {value === '/' && <Boardpage bookCardInfo={bookCardInfoSample}/> } */}
-      {/* {value === 'Chatting' && <Favorites />}
-      {value === 'List' && <Location />}
-      {value === 'Mypage' && <Location />} */}
-
       <BottomNavigation sx={{
-        position:"fixed",
-        bottom:0,
-        left:0, right:0,
         height:"125px",
         borderTop: "1px solid black",
         margin: "0 auto",
