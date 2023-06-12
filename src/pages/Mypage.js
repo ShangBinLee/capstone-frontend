831fa5319ef264037dddc14f1b9cfe49c009573b
LeeShangBin <dkfkgks64@gmail.com>
2023-06-13 07:16:59 +0900

    feature : Implement Mypage component

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MyPageTemplate from '../components/MypageTemplate.js';
 import Header from '../headfoot/header.js';
 import Footer from '../headfoot/footer.js'
-// import MyPageForm from '../mypcomponents/MypageForm.js';
-// import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import { fetchMyUserInfo } from '../lib/fetch_user.js';
 
 
-const Mypage = () => {
+const Mypage = ({rootUrl}) => {
+    const [ userName, setUserName ] = useState('');
+
+    useEffect(() => {
+      fetchMyUserInfo(rootUrl)
+      .then(({userName}) => setUserName(userName));
+    });
     return(
         <body>
             <Header/>
-            <MyPageTemplate/>
+            <MyPageTemplate userName={userName}/>
             <Footer/>
         </body>
     )
